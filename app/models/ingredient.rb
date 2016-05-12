aeb61c60f15f01893e9fe73d23f5c0d3b9974c63
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-12 14:29:23 +0200

    fixed syntax error #3

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ActiveRecord::Base
   validates :name, presence :true, uniqueness :true
 
   has_many :doses, dependent: :destroy
-  has_many :cocktails through :doses
+  has_many :cocktails, through :doses
 end
