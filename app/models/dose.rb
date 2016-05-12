148e101a3b874d15822f3403ff59a44d94e393b4
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-12 11:59:52 +0200

    Mr Cocktail 1 step

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ActiveRecord::Base
+  validates :description, :cocktail, :ingredient presence :true
+  validates_uniqueness_of :cocktail_id :scope => [:ingredient_id]
+
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+class Ingredient < ActiveRecord::Base
+  validates :name, presence :true, uniqueness :true
+
+  has_many :doses dependent: :destroy
+  has_many :cocktails through :doses
+end
