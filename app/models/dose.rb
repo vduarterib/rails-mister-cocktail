db29f68ecf285026a4ea7d572f921231fe965eb8
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-12 14:19:51 +0200

    fixed syntax error

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,6 +1,6 @@
 class Dose < ActiveRecord::Base
-  validates :description, :cocktail, :ingredient presence :true
-  validates_uniqueness_of :cocktail_id :scope => [:ingredient_id]
+  validates :description, :cocktail, :ingredient presence: :true
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
 
   belongs_to :cocktail
   belongs_to :ingredient
